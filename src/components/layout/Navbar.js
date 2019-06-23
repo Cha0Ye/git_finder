fba4a13eb4cf8952d734de4567c26b6489b05ba1
Chao Ye <Chao12.ye@gmail.com>
2019-06-23 14:22:41 -0700

    factored Navbar to a functional component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navbar = ({icon, title}) => {
+const Navbar = ({ icon, title}) => {
         
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                  <i className={icon} /> {title}
-                </h1>
-            </nav>
-        );
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+        </nav>
+    )
 };
 
 Navbar.defaultProps = {
