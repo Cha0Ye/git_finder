c52f1968a686a62b7b5d572983ad61c668f8f583
Chao Ye <Chao12.ye@gmail.com>
2019-06-24 10:37:42 -0700

    add search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'
-import axios from 'axios'
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
