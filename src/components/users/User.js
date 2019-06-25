e5b220b9479619b046ed394266601db0f1cd9964
Chao Ye <Chao12.ye@gmail.com>
2019-06-25 15:57:32 -0700

    add proptype to User component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropType from 'prop-types';
+import { pbkdf2 } from 'crypto';
 
 class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
     }
-
+    static propTypes = {
+        loading: PropType.bool,
+        user: PropType.object.isRequired,
+        getUser: PropType.func.isRequired,
+    }
     render() {
         const { name, avatar_url, location, bio, blog, login, html_url, followers,
                 following, public_repos, public_gists, hireable} = this.props.user;
