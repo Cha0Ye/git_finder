58cfe60a431a8fd446028fc37550c54db6fbadd1
Chao Ye <Chao12.ye@gmail.com>
2019-06-24 19:47:03 -0700

    add react router, create home page, add About component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 
@@ -50,18 +51,28 @@ class App extends Component {
   render() {  
     const { users, loading } = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <Alert alert={this.state.alert} />
-        <Search searchUsers={this.searchUsers} 
-                clearUsers={this.clearUsers} 
-                showClear={users.length > 0 ? true: false } 
-                setAlert={this.setAlert} />
-        <div className="container">
-          <Users loading={loading} 
-                 users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Alert alert={this.state.alert} />
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Fragment>
+                <Search searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={users.length > 0 ? true: false } 
+                    setAlert={this.setAlert} 
+                />
+              </Fragment>
+            )} />
+            <Route exact path = '' /> 
+          </Switch>
+          
+          <div className="container">
+            <Users loading={loading} users={users} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
